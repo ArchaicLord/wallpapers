96f826a786a5188131b2c5209f6b569f1154145d
ArchaicLord <archaiclord@gmail.com>
2023-05-29 10:01:05 +0100

    check workign dir when script is called

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+
 echo "Setting up Archaic Lord's wallpapers"
+echo "current_dir= $(pwd)"
 
 LINUX_DISTRO=$(grep PRETTY_NAME /etc/*release | cut -d "=" -f 2 | awk '{print $1}')
 
