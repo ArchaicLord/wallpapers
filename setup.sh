7614d8ecaf27d4bcbbf29f48475063ae672769c6
ArchaicLord <archaiclord@gmail.com>
2023-05-29 10:54:19 +0100

    moved images to directory

--- setup.sh
+++ setup.sh
@@ -16,6 +16,6 @@ case $LINUX_DISTRO in
     Fedora)
 	echo "Fedora detected"
 	echo "Linking wallpapers from $(pwd)/wallpapers to /usr/share/backgrounds/"
-	sudo ln -s $(pwd)/wallpapers/ /usr/share/backgrounds/
+	sudo ln -s $(pwd)/wallpapers/backgrounds ~/Pictures/
 	;;    
 esac
