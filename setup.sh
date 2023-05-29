dc7dd1e452ed0a8fcbb3d002dd2db5e2575e3110
ArchaicLord <archaiclord@gmail.com>
2023-05-29 10:26:18 +0100

    get pretty distro name

--- setup.sh
+++ setup.sh
@@ -7,7 +7,8 @@ echo "WARNING this script requries SUDO to link wallpaers to system locations"
 
 echo "current_dir= $(pwd)"
 
-LINUX_DISTRO=$(grep PRETTY_NAME /etc/*release | cut -d "=" -f 2 | awk '{print $1}')
+LINUX_DISTRO=$( hostnamectl | grep -i "Operating System" | awk '{print $3}'
+)
 
 echo "LINUX_DISTRO=${LINUX_DISTRO}"
 
@@ -16,7 +17,5 @@ case $LINUX_DISTRO in
 	echo "Fedora detected"
 	echo "Linking wallpapers from $(pwd) to /usr/share/backgrounds/"
 	sudo ln -s $pwd/wallpapers/ /usr/share/backgrounds/
-
-	;;
-    
+	;;    
 esac
